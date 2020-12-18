2115689f9b272626ed97c9c1112a138fc75f9152
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-18 16:38:48 -0600

    OESS-104:Create a new lzf repo.

---
+++ config/cmake/cacheinit.cmake
@@ -0,0 +1,15 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+set (BUILD_SHARED_LIBS ON CACHE BOOL "Build Shared Libraries" FORCE)
+
+set (CMAKE_INSTALL_FRAMEWORK_PREFIX "Library/Frameworks" CACHE STRING "Frameworks installation directory" FORCE)
+
+set (LZF_PACKAGE_EXT "" CACHE STRING "Name of package extension" FORCE)
+
+set (BUILD_EXAMPLES ON CACHE BOOL "Build LZF Library Examples" FORCE)
+
+set (BUILD_TESTING ON CACHE BOOL "Build LZF Unit Testing" FORCE)
